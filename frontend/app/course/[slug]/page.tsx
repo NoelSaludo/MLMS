22aa070f9772a43655dca226a88fd79b45295015
NoelSaludo <noelsaludo17@gmail.com>
2026-07-14 08:40:30 +0800

    reworked the drop down menu

--- frontend/app/course/[slug]/page.tsx
+++ frontend/app/course/[slug]/page.tsx
@@ -1,62 +1,41 @@
 'use client'
 
-import { use, useEffect, useState } from "react";
-import Sidebar from "@/components/shared/Sidebar";
-import useSession from "@/hooks/useSession";
-import CourseTitleCard from "@/components/course/CourseTitleCard";
-import CourseDashboard from "@/components/course/CourseDashboard";
-import CourseDropdownAction from "@/components/course/CourseDropdownAction";
 import ContentCreationModal from "@/components/course/ContentCreationModel";
-import { getCourseById } from "@/lib/dal";
+import CourseDropdownAction from "@/components/course/CourseDropdownAction";
+import Sidebar from "@/components/shared/Sidebar";
+import { useState, useEffect, use } from "react";
 
-export default function CoursePage({
-    params,
-}: {
-    params: Promise<{ slug: string }>
-}) {
-    const session = useSession().session;
+
+export default function CoursePage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = use(params);
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [modalOpen, setModalOpen] = useState(false);
-    const [contentType, setContentType] = useState<'announcement' | 'material' | 'assignment' | null>(null);
+    const [contentToCreate, setContentToCreate] = useState<'announcement' | 'material' | 'assignment'>('announcement');
     const [courseData, setCourseData] = useState<any>(null);
+    const [role, setRole] = useState<string | null>(null);
 
     useEffect(() => {
-        const fetchCourseData = async () => {
-            const res = await fetch(`/api/courses/${slug}`);
-            const data = await res.json();
-            setCourseData(data.course);
-        };
-        fetchCourseData();
-    }, [slug]);
-    
-    if (!courseData) {
-        return <div>Loading...</div>;
-    }
 
-    console.log('Course Data:', courseData);
+    })
 
     return (
         <div className="grid grid-cols-4 h-screen w-full overflow-hidden justify-center">
             <Sidebar />
             <div className="col-span-3 p-4 flex flex-col min-h-0">
-                {session?.role === 'Teacher' && (
-                    // dropdown menu for uploading and creating new course content
-                    <div className="flex gap-4 mb-4">
-                        <div className="flex flex-row items-center gap-2">
-                            <a href="/" className="bg-gray-500 text-white px-4 py-2 rounded">
-                                Go Back
-                            </a>
-                            <button
-                                className="bg-blue-500 text-white px-4 py-2 rounded"
-                                onClick={() => setDropdownOpen(true)}>
-                                Create new course content
-                            </button>
-                        </div>
+                <div className="flex gap-4 mb-4">
+                        <a href="/" className="bg-gray-500 text-white px-4 py-2 rounded">
+                            Go Back
+                        </a>
+                    <div className="flex-1 text-end">
+                        <button
+                            className="bg-blue-500 text-white px-4 py-2 rounded"
+                            onClick={() => setDropdownOpen(true)}>
+                            Create new course content
+                        </button>
                         {dropdownOpen && (
                             <CourseDropdownAction onCreateContent={
                                 (type) => {
-                                    setContentType(type);
+                                    setContentToCreate(type);
                                     setModalOpen(true);
                                 }}
                                 setDropdownOpen={setDropdownOpen} />
@@ -64,14 +43,12 @@ export default function CoursePage({
                         {modalOpen && (
                             <ContentCreationModal
                                 onClose={() => setModalOpen(false)}
-                                type={contentType!}
+                                type={contentToCreate!}
                                 courseId={slug} />
                         )}
                     </div>
-                )}
-                <CourseTitleCard title={courseData.title} description={courseData.description} />
-                <CourseDashboard courseId={parseInt(slug)} />
+                </div>
             </div>
         </div>
-    );
+    )
 }
--- frontend/components/main/CourseCatalogue.tsx
+++ frontend/components/main/CourseCatalogue.tsx
@@ -4,20 +4,24 @@ import LoadingComponent from '../shared/LoadingPage';
 import ErrorComponent from '../shared/ErrorComponent';
 import { useLoadUsersCourses } from '@/hooks/useLoadUsersCourses';
 import CreateOrJoinCourseButtonComponent from '../course/CreateOrJoinCourseButton';
+import { useRouter } from 'next/navigation';
 
 export default function CourseCatalogue({ userId, role }: { userId: string; role: string }) {
     const { loading, courses, error } = useLoadUsersCourses(userId);
+    const router = useRouter();
 
     if (loading) return LoadingComponent();
     if (error) return ErrorComponent({ error });
-    
+
     return (
         <div className="col-span-3 p-4">
             <CreateOrJoinCourseButtonComponent role={role} />
 
             <div className="flex flex-wrap align-items justify-content gap-4 mt-4">
                 {courses.length > 0 && courses.map((course) => (
-                    <a href={`/course/${course.course_id}`} key={course.course_id} className="w-full md:w-1/2 lg:w-1/3">
+                    <a onClick={() => router.push(`/course/${course.course_id}`)}
+                        key={course.course_id}
+                        className="w-full md:w-1/2 lg:w-1/3">
                         <div className="rounded-lg border border-gray-300 p-4 shadow hover:shadow-lg transition-shadow duration-300">
                             <h3 className="text-xl font-semibold">{course.title}</h3>
                             <p className="mt-2 text-gray-600">{course.description}</p>
